9b6512dbc62f070986c09e3ff8c0d3b2b6dffe12
Kelly <kdsl.dantas2017@gmail.com>
2023-09-04 09:09:32 -0300

    Exerício 03

--- listadeExercicio01.sql
+++ listadeExercicio01.sql
@@ -2,3 +2,8 @@ select titulo from livros;
 
 select nome from autores 
 where nascimento < '1900-01-01';
+
+select livros.titulo, autores.nome
+from livros 
+left join autores on autor.id = livros.autor_id;
+
