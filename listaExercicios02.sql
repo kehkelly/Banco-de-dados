84127e77b45c27d58ebd7810b0b491b91abe035b
Kelly <kdsl.dantas2017@gmail.com>
2023-09-25 20:53:28 -0300

    Exercício 03

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -14,3 +14,10 @@ call sp_LivrosPorCategoria('Ficção Científica');
 call sp_LivrosPorCategoria('Romance');
 call sp_LivrosPorCategoria('Ciência');
 
+create procedure sp_ContarLivrosPorCategoria(in p_categoria varchar(255), out @total_livros int)
+begin
+    select count(*) into total_livros from livros where categoria = p_categoria;
+end //
+call sp_ContarLivrosPorCategoria('Ficção', @total_livros);
+select @total_livros;
+
