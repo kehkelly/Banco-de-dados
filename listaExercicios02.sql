9048b10e7b7bf9aba5161507899afcbc1a0c2c16
Kelly <kdsl.dantas2017@gmail.com>
2023-09-25 20:48:02 -0300

    Exercício 02

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -4,5 +4,13 @@ begin
 select Nome from autor;
 end
 //
-
 call sp_ListarAutores ();
+
+create procedure sp_LivrosPorCategoria(in p_categoria varchar(255))
+begin
+    select * from livros where categoria = p_categoria;
+end //
+call sp_LivrosPorCategoria('Ficção Científica');
+call sp_LivrosPorCategoria('Romance');
+call sp_LivrosPorCategoria('Ciência');
+
