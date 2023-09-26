ed70738a7eea66dc98fbc1b63767b8b45f247ee3
Kelly <kdsl.dantas2017@gmail.com>
2023-09-25 21:05:25 -0300

    Exercício 04

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -21,3 +21,18 @@ end //
 call sp_ContarLivrosPorCategoria('Ficção', @total_livros);
 select @total_livros;
 
+create procedure sp_VerificarLivrosCategoria(in p_categoria varchar(255), out categoria_tem_livros boolean)
+begin
+    declare total_livros int;
+    
+    select count(*) into total_livros from livros where categoria = p_categoria;
+    
+    if total_livros > 0 then
+        set categoria_tem_livros = TRUE;
+    else
+        set categoria_tem_livros = FALSE;
+    end if;
+end //
+call sp_VerificarLivrosCategoria('Ficção', @categoria_tem_livros);
+select @categoria_tem_livros;
+
