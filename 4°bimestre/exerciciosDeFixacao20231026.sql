45ab4406d4484238eb4df0fb2025419af53cdbb6
Dantas <102705107+kehkelly@users.noreply.github.com>
2023-10-26 18:13:09 -0300

    Create exerciciosDeFixacao20231026.sql

---
+++ 4°bimestre/exerciciosDeFixacao20231026.sql
@@ -0,0 +1,4 @@
+CREATE TRIGGER after_auditoria_insert AFTER INSERT ON Clientes FOR EACH ROW
+INSERT INTO Auditoria(mensagem) VALUES ('Mensagem inserida');
+INSERT INTO Clientes (nome) VALUES ('Carlos');
+SELECT * from Auditoria;
