dcc5b637645097e1385f0799c99fd53a0f2fcce9
Dantas <102705107+kehkelly@users.noreply.github.com>
2023-10-26 22:46:57 -0300

    Update ExerciciosDeFixacao20231026.sql

--- 4°bimestre/ExerciciosDeFixacao20231026.sql
+++ 4°bimestre/ExerciciosDeFixacao20231026.sql
@@ -6,4 +6,10 @@ SELECT * fROM Auditoria;
 CREATE TRIGGER before_cliente_delete BEFORE DELETE ON Clientes FOR EACH ROW
 INSERT INTO Auditoria(mensagem) VALUES ('Exclusão');
 DELETE FROM Clientes WHERE id = 3;
+SELECT * FROM Auditoria;
+
+CREATE TRIGGER after_cliente_update AFTER UPDATE ON Clientes FOR EACH ROW
+INSERT INTO Auditoria (mensagem, nome_antigo, nome_novo) VALUES ('update', OLD.nome, NEW.nome);
+UPDATE Clientes SET nome = 'Phan' WHERE id = 2;
 SELECT * FROM Clientes;
+SELECT * FROM Auditoria;
